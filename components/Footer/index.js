cc73996eb8b9d65d69db0459e730d1f2099889ee
stevenorell <design@stevenorell.com>
2022-04-18 20:58:27 -0600

    Tailwindified footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,11 +1,9 @@
-import styles from './styles.module.css';
-
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <span className={styles.legal}>&copy; always</span>
-      <a className={styles.link} href="/steve-norell-resume-2022.pdf" download>resume</a>
-      <a className={styles.link} href="https://www.linkedin.com/in/stevenorell/" target="_blank" rel="noopener noreferrer">linkedin</a>
+    <footer className="text-2xl flex flex-col md:flex-row items-center py-10">
+      <span className="block mb-4 md:mr-10">&copy; always</span>
+      <a className="block mb-4 md:mr-10 text-fuchsia-600 underline" href="/steve-norell-resume-2022.pdf" download>resume</a>
+      <a className="block mb-4 md:mr-10 text-fuchsia-600 underline" href="https://www.linkedin.com/in/stevenorell/" target="_blank" rel="noopener noreferrer">linkedin</a>
     </footer>
   )
 }
